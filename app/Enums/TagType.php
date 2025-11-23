178fdb9f4c4a2fa702b4cbebf06802c980157b31
Blede <bledegraphics@gmail.com>
2025-11-22 20:53:08 -0500

    fix: Updated available tags

--- app/Enums/TagType.php
+++ app/Enums/TagType.php
@@ -5,4 +5,6 @@ namespace App\Enums;
 enum TagType: string
 {
     case BLUEPRINT_TAGS = 'blueprint_tags';
+    case BLUEPRINT_TIER = 'blueprint_tier';
+    case BLUEPRINT_TYPE = 'blueprint_type';
 }
